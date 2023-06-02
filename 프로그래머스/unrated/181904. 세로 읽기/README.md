9ba8167f797ce2ae6e21fd85916e0f2cf35c18ae
JIWON27 <mjo4260@naver.com>
2023-06-03 08:57:03 +0900

    [unrated] Title: 세로 읽기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/README.md
+++ 프로그래머스/unrated/181904. 세로 읽기/README.md
@@ -1,6 +1,6 @@
 # [unrated] 세로 읽기 - 181904 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181904#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181904) 
 
 ### 성능 요약
 
--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
@@ -1,8 +1,2 @@
 def solution(my_string, m, c):
-    if m == 1:
-        return my_string
-    string = ''
-    answer = [my_string[i:i+m] for i in range(0, len(my_string), m)]
-    for i in answer:
-        string += i[c-1]
-    return string
+    return my_string[c-1::m]
