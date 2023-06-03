48a993271ad6981898bcc2a52b65414c527207fb
JIWON27 <mjo4260@naver.com>
2023-06-03 10:15:56 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.79 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -2,6 +2,5 @@ from itertools import combinations
 def solution(numbers):
     answer = []
     for i in combinations(numbers,2):
-        if sum(i) not in answer:
-            answer.append(sum(i))
-    return sorted(answer)
+        answer.append(sum(i))
+    return sorted(list(set(answer)))
