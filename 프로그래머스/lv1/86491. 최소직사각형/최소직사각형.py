b5a4c4a730fa5c5d41939877912e1d6f6d3a6511
JIWON27 <mjo4260@naver.com>
2023-06-11 16:55:23 +0900

    Update 최소직사각형.py

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -8,3 +8,13 @@ def solution(sizes):
     sizes = sorted(sizes, key=lambda x:x[1])
     h_max = sizes[len(sizes)-1][1]
     return w_max * h_max
+
+# def solution(sizes):
+#   w_max = 0
+#   h_max = 0
+#   for i in range(len(sizes)):
+#       if sizes[i][0] < sizes[i][1]:
+#           sizes[i][0], sizes[i][1] = sizes[i][1], sizes[i][0]
+#       w_max = max(w_max,sizes[i][0] )
+#       h_max = max(h_max,sizes[i][1] )
+#   return w_max * h_max 
