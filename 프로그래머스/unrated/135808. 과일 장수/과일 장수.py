93006cd8876e88feb92fdd111e21e9ade2238eba
JIWON27 <mjo4260@naver.com>
2023-06-13 12:44:03 +0900

    [unrated] Title: 과일 장수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -2,8 +2,7 @@ def solution(k, m, score):
     answer = 0
     score.sort()
     if len(score) % m == 0:
-        score = score[::m]
-        answer = sum(score) * m
+        answer = sum(score[::m]) * m
     else:
         answer = sum(score[-m::-m]) * m
     return answer
