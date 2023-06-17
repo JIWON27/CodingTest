ff2f81d27622f9209afeba3a0c7c725ff7565384
JIWON27 <mjo4260@naver.com>
2023-06-17 14:21:04 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -0,0 +1,15 @@
+def solution(id_list, report, k):
+    answer = [0 for _ in range(len(id_list))]
+    dict_ = {}
+    for users in report:
+        if users.split()[1] not in dict_.keys():
+            dict_[users.split()[1]] = [users.split()[0]]
+        else:
+            if users.split()[0] not in dict_.get(users.split()[1]):
+                dict_[users.split()[1]] += [users.split()[0]]
+    for users in dict_.values():
+        if len(users) >= k:
+            for user in users:
+                idx = id_list.index(user)
+                answer[idx] += 1
+    return answer
