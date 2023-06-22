bec2d41e64f51cba9495070e33610087e319df1d
JIWON27 <mjo4260@naver.com>
2023-06-22 18:02:02 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -0,0 +1,9 @@
+
+from collections import deque 
+def solution(n, lost, reserve):
+    students = n-len(lost)
+    plus = 0
+    print(set(lost) - set(reserve))
+    
+    
+    return students + plus
