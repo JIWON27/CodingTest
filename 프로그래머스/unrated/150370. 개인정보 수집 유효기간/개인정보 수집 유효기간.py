595eb6a456a59a0d297cf859e81626937171972a
JIWON27 <mjo4260@naver.com>
2023-07-20 08:15:16 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.16 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -0,0 +1,23 @@
+def solution(today, terms, privacies):
+    answer = []
+    term = {i[0] : i[2:] for i in terms}
+    
+    today = today.split(".")
+    
+    for idx, privacy in enumerate(privacies):
+        day = privacy.split(" ")
+        date = day[0].split(".")
+        
+        expiry =  int(date[1]) + int(term.get(day[1]))
+            
+        newTodayYear = date[0]
+        newTodayMonth = 0
+        
+        newTodayMonth = ( int(today[0]) - int(date[0]) ) * 12 + int(today[1])
+        
+        if newTodayMonth > expiry:
+            answer.append(idx+1) 
+        elif newTodayMonth == expiry:
+            if int(today[2]) >= int(date[2]):
+                answer.append(idx+1) 
+    return answer
