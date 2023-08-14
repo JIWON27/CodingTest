ab754da1b51d17c0d21721e3751e9bb28c915e02
Jiwon <mjo4260@naver.com>
2023-08-14 15:32:06 +0900

    [level 2] Title: 카펫, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,13 +1,8 @@
 def solution(brown, yellow):
-    answer = []
-    if yellow == 1:
-        return [3,3]
-    else:
-        number = yellow
-        while True:
-            if yellow % number == 0: # 약수면
-                print( 2 * (number + 2 + (yellow // number))) 
-                if  2 * (number + 2 + (yellow // number)) == brown:
-                    break;
-            number -= 1
+    number = yellow
+    while True:
+        if yellow % number == 0: # 약수면
+            if  2 * (number + 2 + (yellow // number)) == brown:
+                break;
+        number -= 1
     return [number+2, yellow // number+2]
