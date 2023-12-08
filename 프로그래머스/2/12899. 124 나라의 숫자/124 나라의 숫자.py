1a6052207437e1c53f40a4cd492b0aed1f160966
Jiwon <mjo4260@naver.com>
2023-12-08 18:06:38 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
@@ -1,9 +1,8 @@
 def solution(n):
     answer = ''
-    arr = ['4','1','2']
     while n > 0:
         if n % 3 == 0:
-            n = n//3-1 # 몫에서 -1, 나머지가 0이 되면 안되니까
+            n = n//3-1 
             answer += '4'
         else:
             answer += str(n%3)
